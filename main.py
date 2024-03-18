a3ebaf90e6121a6a23f7040d7e8c57ad9896f29c
Knight532 <knight.olegsum41@gmail.com>
2024-03-18 17:14:09 +0200

    Повністю працююча анімація гравця

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ player_size = (91, 161)
 player_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\Visual Game\\player.png').convert_alpha(), player_size)
 player_rect = player_image.get_rect()
 player_move_up = [0, -6]  # Збільшимо значення швидкості при стрибку
-player_move_left = [-3, 0]
-player_move_right = [3, 0]
+player_move_left = [-2, 0]
+player_move_right = [2, 0]
 
 player_rect.centerx = WIDTH // 5.5  # По горизонталі в центрі
 player_rect.bottom = HEIGHT // 1.25
@@ -37,6 +37,7 @@ background_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\
 
 playing = True
 is_jumping = False
+animate_player = False
 jump_count = 10  # Кількість кадрів, протягом яких гравець пригаватиме
 image_index = 0
 
@@ -51,19 +52,27 @@ while playing:
                 jump_count = 30  # Встановлюємо кількість кадрів стрибка
 
         if event.type == CHANGE_IMAGE:
-            player_image = pygame.image.load(os.path.join(IMAGE_PATH, PLAYER_IMAGES[image_index]))
-            player_image = pygame.transform.scale(player_image, player_size)  # Змінюємо розмір зображення
-            image_index += 1
-            if image_index >= len(PLAYER_IMAGES):
-                image_index = 0
+            if animate_player:  # Перевіряємо, чи потрібно змінювати зображення гравця
+                player_image = pygame.image.load(os.path.join(IMAGE_PATH, PLAYER_IMAGES[image_index]))
+                player_image = pygame.transform.scale(player_image, player_size)  # Змінюємо розмір зображення
+                image_index += 1
+                if image_index >= len(PLAYER_IMAGES):
+                    image_index = 0
 
     keys = pygame.key.get_pressed()
 
+    # Обробка руху гравця
     if keys[K_LEFT] and player_rect.left > 0:
         player_rect = player_rect.move(-3, 0)  # Рух вліво
+        animate_player = True  # Встановлюємо прапорець анімації
 
     if keys[K_RIGHT] and player_rect.right < WIDTH:
         player_rect = player_rect.move(3, 0)  # Рух вправо
+        animate_player = True  # Встановлюємо прапорець анімації
+
+    # Якщо гравець не рухається, встановлюємо прапорець анімації в False
+    if not keys[K_LEFT] and not keys[K_RIGHT]:
+        animate_player = False
 
     # Обробка стрибка
     if is_jumping:
@@ -73,7 +82,6 @@ while playing:
         else:
             jump_count = 30
             is_jumping = False
-
 
     # Відображення фону
     main_display.blit(background_image, (0, 0))
