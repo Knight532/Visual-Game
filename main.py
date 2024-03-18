3bc617496125344bab22502957a9b29a21db1470
Knight532 <knight.olegsum41@gmail.com>
2024-03-18 17:01:37 +0200

    Дадав анімацію гравця

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import pygame
-import random
 from pygame.constants import QUIT, K_LEFT, K_RIGHT, K_UP
 
 pygame.init()
@@ -14,11 +13,17 @@ FPS = pygame.time.Clock()
 HEIGHT = 800
 WIDTH = 1200
 
+
 FONT = pygame.font.SysFont('Verdana', 20)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Завантаження зображення гравця
+IMAGE_PATH = "animation_player"
+PLAYER_IMAGES = os.listdir(IMAGE_PATH)
+CHANGE_IMAGE = pygame.USEREVENT + 1
+pygame.time.set_timer(CHANGE_IMAGE, 200)
+
 player_size = (91, 161)
 player_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\Visual Game\\player.png').convert_alpha(), player_size)
 player_rect = player_image.get_rect()
@@ -33,33 +38,42 @@ background_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\
 playing = True
 is_jumping = False
 jump_count = 10  # Кількість кадрів, протягом яких гравець пригаватиме
+image_index = 0
 
 while playing:
     FPS.tick(120)
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
-        elif event.type == pygame.KEYDOWN:  # Перевіряємо натискання клавіші
-            if event.key == pygame.K_UP and not is_jumping:  # Якщо натиснута клавіша вгору і гравець не стрибає вже
+        elif event.type == pygame.KEYDOWN:  
+            if event.key == pygame.K_UP and not is_jumping:  
                 is_jumping = True
                 jump_count = 30  # Встановлюємо кількість кадрів стрибка
+
+        if event.type == CHANGE_IMAGE:
+            player_image = pygame.image.load(os.path.join(IMAGE_PATH, PLAYER_IMAGES[image_index]))
+            player_image = pygame.transform.scale(player_image, player_size)  # Змінюємо розмір зображення
+            image_index += 1
+            if image_index >= len(PLAYER_IMAGES):
+                image_index = 0
 
     keys = pygame.key.get_pressed()
 
     if keys[K_LEFT] and player_rect.left > 0:
-        player_rect = player_rect.move(player_move_left)
+        player_rect = player_rect.move(-3, 0)  # Рух вліво
 
     if keys[K_RIGHT] and player_rect.right < WIDTH:
-        player_rect = player_rect.move(player_move_right)
+        player_rect = player_rect.move(3, 0)  # Рух вправо
 
     # Обробка стрибка
     if is_jumping:
-        if jump_count >= -30:  # Зупиняємо стрибок після певної кількості кадрів
-            player_rect.y -= (jump_count * abs(jump_count)) * 0.03  # Формула для анімації при стрибку
+        if jump_count >= -30:  
+            player_rect.y -= (jump_count * abs(jump_count)) * 0.03  
             jump_count -= 1
         else:
             jump_count = 30
             is_jumping = False
+
 
     # Відображення фону
     main_display.blit(background_image, (0, 0))
