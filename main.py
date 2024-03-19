93613189e9a450a6a60c0f7f0101fe4cc06a1673
Knight532 <knight.olegsum41@gmail.com>
2024-03-19 11:38:47 +0200

    Додав ворога з анімацією

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ PLAYER_IMAGES = os.listdir(IMAGE_PATH)
 CHANGE_IMAGE = pygame.USEREVENT + 1
 pygame.time.set_timer(CHANGE_IMAGE, 200)
 
+# Завантаження зображення ворога
+ENEMY_IMAGE_PATH = "animation_enemy"
+ENEMY_IMAGES = os.listdir(ENEMY_IMAGE_PATH)
+ENEMY_CHANGE_IMAGE = pygame.USEREVENT + 2
+pygame.time.set_timer(ENEMY_CHANGE_IMAGE, 200)
+
+# Створення гравця
 player_size = (91, 161)
 player_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\Visual Game\\player.png').convert_alpha(), player_size)
 player_rect = player_image.get_rect()
@@ -28,11 +35,25 @@ player_move_up = [0, -6]  # Збільшимо значення швидкост
 player_move_left = [-2, 0]
 player_move_right = [2, 0]
 player_health = 100
-player_attack = random.randint(5, 50)
+player_attack = random.randint(5, 20)
+
+#Створення ворога
+enemy_size = (91, 161)
+enemy_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\Visual Game\\enemy.png').convert_alpha(), player_size)
+enemy_rect = enemy_image.get_rect()
+enemy_health = 100
+enemy_attack = random.randint(5, 20)
 
 player_rect.centerx = WIDTH // 5.5  # По горизонталі в центрі
 player_rect.bottom = HEIGHT // 1.25
 background_image = pygame.transform.scale(pygame.image.load('D:\\Dev\\Projects\\Visual Game\\background.png').convert_alpha(), (WIDTH, HEIGHT))
+
+# Початкове положення ворога
+enemy_rect.centerx = WIDTH // 1.2  # По горизонталі в правому куті
+enemy_rect.bottom = HEIGHT // 1.25
+
+animate_enemy = True
+enemy_image_index = 0
 
 playing = True
 is_jumping = False
@@ -77,6 +98,14 @@ while playing:
                 if image_index >= len(PLAYER_IMAGES):
                     image_index = 0
 
+        if event.type == ENEMY_CHANGE_IMAGE:
+            if animate_enemy:  # Перевіряємо, чи потрібно змінювати зображення ворога
+                enemy_image = pygame.image.load(os.path.join(ENEMY_IMAGE_PATH, ENEMY_IMAGES[enemy_image_index]))
+                enemy_image = pygame.transform.scale(enemy_image, enemy_size)  # Змінюємо розмір зображення
+                enemy_image_index += 1
+                if enemy_image_index >= len(ENEMY_IMAGES):
+                    enemy_image_index = 0
+
     keys = pygame.key.get_pressed()
 
     # Обробка руху гравця
@@ -87,6 +116,12 @@ while playing:
     if keys[K_RIGHT] and player_rect.right < WIDTH:
         player_rect = player_rect.move(3, 0)  # Рух вправо
         animate_player = True  # Встановлюємо прапорець анімації
+
+    if animate_enemy:
+        if enemy_rect.left > WIDTH:
+            enemy_rect.right = 0
+        else:
+            enemy_rect = enemy_rect.move(-2, 0)  # Рух ворога вліво
 
     # Якщо гравець не рухається, встановлюємо прапорець анімації в False
     if not keys[K_LEFT] and not keys[K_RIGHT]:
@@ -106,6 +141,9 @@ while playing:
     
     # Відображення гравця
     main_display.blit(player_image, player_rect)
+
+    #Відображення ворога
+    main_display.blit(enemy_image, enemy_rect)
 
     draw_health_bar(main_display, player_health)
 
